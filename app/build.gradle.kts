4b325f87256594c5745ceab8f5638a416ec9b485
Muhammed Cosan <muha0653@edu.easj.dk>
2024-05-29 14:11:07 +0200

    Tilføjet "Login" klasserne.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.databinding:databinding-runtime:8.4.1")
     implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
